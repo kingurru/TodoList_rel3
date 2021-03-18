1bdd2d833d901517d4f21cba97ad68bbc57f923d
kingurru <kingnikoriginal@gmail.com>
2021-03-18 21:00:27 +0300

    feat: add columns

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,11 @@ module.exports = {
                         presets: ['@babel/preset-env']
                     }
                 }
-            }
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
         ]
     }
 
