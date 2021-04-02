da8ce66f3e266970cf0589db1580c90740ae828c
kingurru <kingnikoriginal@gmail.com>
2021-04-02 11:30:44 +0300

    feat: add animation & add png

--- src/constructor.js
+++ src/constructor.js
@@ -1,3 +1,5 @@
+import png from './images/delete.png'
+
 export class TaskItem {
   constructor(tagName = 'li') {
     this.$item = document.createElement(tagName)
@@ -15,19 +17,28 @@ export default class Task extends TaskItem {
     this.$box = document.querySelector(`.${this.box}`)
 
     this.$label = document.createElement("label")
+    this.$img = document.createElement("img")
+    this.$img.setAttribute("src", png)
     this.$checkbox = document.createElement("input")
     this.$checkbox.setAttribute("type", "checkbox")
     this.$span = document.createElement("span")
     this.$span.textContent = this.name
 
+
     this.$item.appendChild(this.$label)
     this.$label.appendChild(this.$checkbox)
     this.$label.appendChild(this.$span)
+    this.$item.appendChild(this.$img)
     this.$box.appendChild(this.$item)
 
     console.log('Create task')
     return this
   }
+
+  delete() {
+    this.$item.remove()
+    console.log('Delete task')
+}
 
   info() {
     console.log('Information', this)
--- src/events.js
+++ src/events.js
@@ -5,6 +5,14 @@ const form = document.querySelector('form')
 const options = document.querySelectorAll('option')
 const titles = document.querySelectorAll('.title')
 
+const test = document.querySelector('.test')
+
+test.addEventListener('click', (event) => {
+  console.log(store.green[0].delete())
+
+})
+
+
 titles.forEach(title => title.addEventListener('click', function (event) {
   toggleVisibilityBox(event.target.parentElement)
 }))
